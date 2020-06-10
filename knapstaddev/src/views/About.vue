58619edf116392e079d02442a0f8dccb1fe730f5
Bendik Knapstad <knaben@OBOS.NO>
2020-06-10 12:41:50 +0200

    move dir

--- knapstaddev/src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-      <vue-headful
-            title="Title from vue-headful"
-            description="Description from vue-headful"
-        />
-    <h1>This is an about page</h1>
-  </div>
-</template>
