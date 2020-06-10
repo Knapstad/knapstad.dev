7d3ba04ca144769535431102adae25a5abfc224e
Bendik Knapstad <knaben@OBOS.NO>
2020-06-10 12:50:25 +0200

    build site

---
+++ dist/js/about.6db261d1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,a){"use strict";a.r(t);var u=function(){var e=this,t=e.$createElement,a=e._self._c||t;return a("div",{staticClass:"about"},[a("vue-headful",{attrs:{title:"Title from vue-headful",description:"Description from vue-headful"}}),a("h1",[e._v("This is an about page")])],1)},i=[],n=a("2877"),s={},l=Object(n["a"])(s,u,i,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=about.6db261d1.js.map
