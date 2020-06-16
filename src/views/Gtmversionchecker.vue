a03a8b6fc24e953929c114b49d4d298326f9a5e9
Bendik Knapstad <knaben@OBOS.NO>
2020-06-16 14:58:44 +0200

    add gtnversionchecker view

---
+++ src/views/Gtmversionchecker.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <main>
+    <div class="content">
+      <VueShowdown :markdown="fileContent"></VueShowdown>
+    </div>
+  </main>
+</template>
+
+<script>
+import mdFile from '@/assets/Atricles/GtmVersionChecker.md';
+
+export default {
+  name: 'VueMarkdown',
+  data() {
+    return {
+      fileContent: '',
+    };
+  },
+  mounted() {
+    this.getMDFile();
+  },
+  methods: {
+    getMDFile() {
+      this.fileContent = mdFile;
+    },
+  },
+};
+</script>
