b3ea8da3a4dc016b34d0defd955e3889c09fb5ec
Bendik Knapstad <knaben@OBOS.NO>
2020-06-18 09:54:52 +0200

    added vueGtm

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import vueHeadful from 'vue-headful';
 import VueResource from 'vue-resource';
 import VueShowdown from 'vue-showdown';
-
+import VueGtm from 'vue-gtm';
 
 import App from './App.vue';
 import router from './router';
@@ -11,6 +11,18 @@ import router from './router';
 Vue.config.productionTip = false;
 Vue.component('vue-headful', vueHeadful);
 Vue.use(VueResource);
+
+Vue.use(VueGtm, {
+  id: 'GTM-MB4G868',
+  queryParams: { // Add url query string when load gtm.js with GTM ID (optional)
+
+  },
+  enabled: true,
+  debug: true,
+  loadScript: true,
+  vueRouter: router,
+  ignoredViews: [],
+});
 
 Vue.use(VueShowdown, {
   flavor: 'github',
