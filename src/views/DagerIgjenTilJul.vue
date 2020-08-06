e0f7830f0ca83001dc4dddb5eff52a13ef2aaca8
Bendik <knaben@OBOS.NO>
2020-08-06 14:01:57 +0200

    added dagertiljul view, basic impementation

---
+++ src/views/DagerIgjenTilJul.vue
@@ -0,0 +1,28 @@
+<template>
+  <ChristmasCounter date="week" @dateObject="getData($event)" />
+</template>
+
+<script>
+import ChristmasCounter from '@/components/ChristmasCounter.vue';
+
+export default {
+  name: 'Dager igjen til jul',
+  components: {
+    ChristmasCounter,
+  },
+  methods: {
+    getData(data) {
+      this.days = data.days,
+      this.weeks = data.weeks,
+    },
+  },
+  data(){
+    return{
+      days: '',
+      weeks: '',
+    }
+  },
+};
+</script>
+
+<style></style>
