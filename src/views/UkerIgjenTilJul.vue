623d37d56dd8e463b739390331ef706549f05090
Bendik <knaben@OBOS.NO>
2020-08-06 14:32:27 +0200

    added UkerIjgen til jul

---
+++ src/views/UkerIgjenTilJul.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <vue-headful title="Knapstad.dev - Uker Igjen Til Jul" :description="description" />
+    <ChristmasCounter date="week" @dateObject="getData($event)" />
+    <router-link to="/dagerigjentiljul">
+      <p>Lurer du på hvor mange dager det er til jul?</p>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import ChristmasCounter from '@/components/ChristmasCounter.vue';
+
+export default {
+  name: 'Uker igjen til jul',
+  components: {
+    ChristmasCounter,
+  },
+  methods: {
+    getData(data) {
+      this.days = data.days;
+      this.weeks = data.weeks;
+    },
+  },
+  data() {
+    return {
+      days: '',
+      weeks: '',
+      description: 'Det er ' + this.weeks + 'igjen til jul',
+    };
+  },
+};
+</script>
+
+<style></style>
