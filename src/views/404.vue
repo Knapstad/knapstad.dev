ffb74c8c78b801ea17fe555dae08309fac1ba1fd
Bendik <knaben@OBOS.NO>
2020-08-10 12:39:27 +0200

    added 404 view

---
+++ src/views/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="huge">404</div>
+    <div class="large">Not found</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.huge {
+  font-size: 120px;
+}
+.large {
+  font-size: 40px;
+}
+</style>
