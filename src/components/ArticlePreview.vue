eebc7934e46bdd3de165325da822e2cffb249e2e
Bendik <knaben@OBOS.NO>
2020-08-14 12:20:21 +0200

    Added working articlepreview moduel

--- src/components/ArticlePreview.vue
+++ src/components/ArticlePreview.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>
-    <h2>{{ title }}</h2>
-    <p>{{ decription }}</p>
+  <div class="cardbg">
+    <h2>{{ article.title | capitalize }}</h2>
+    <p>{{ article.description | capitalize }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['title', 'description'],
+  name: 'ArticlePreview',
+  props: ['article'],
 };
 </script>
-<style>
+<style scoped>
+h2 {
+  text-align: left;
+  margin: 1px;
+  padding: 2px;
+}
+p {
+  text-align: left;
+  margin-left: 2px;
+  padding-bottom: 5px;
+}
+.cardbg {
+  background: whitesmoke;
+  text-align: left;
+  border-radius: 2px;
+  box-shadow: 2px 2px 5px #000;
+  width: 100%;
+  -webkit-margin-after: 1rem;
+  margin-block-end: 1rem;
+}
 </style>
