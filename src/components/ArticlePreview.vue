2f29c03e602ed685d59c570f99cf205a72f8e30d
Bendik <knaben@OBOS.NO>
2020-08-14 11:01:12 +0200

    added basic artivle preview component

---
+++ src/components/ArticlePreview.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ decription }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description'],
+};
+</script>
+<style>
+</style>
