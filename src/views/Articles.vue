72401e2784f47e05d777017f5524b0285bf56e35
Bendik <knaben@OBOS.NO>
2020-08-14 14:37:06 +0200

    added real data and removed dummydata for articles

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -8,35 +8,14 @@
 
 <script>
 import ArticlePreview from '@/components/ArticlePreview.vue';
+import articles from '@/assets/Atricles/articles.json';
 
 export default {
   name: 'Articles',
   components: { ArticlePreview },
   data() {
     return {
-      articles: [
-        {
-          id: 1,
-          title: 'en tittel',
-          description: 'en liten beskrivelse her da altså',
-        },
-        {
-          id: 2,
-          title: 'en tittel til',
-          description: 'en liten beskrivelse her da min venn altså',
-        },
-        {
-          id: 3,
-          title: 'en tittel her ogsåp',
-          description: 'en liten beskrivelse her og da altså',
-        },
-        {
-          id: 4,
-          title: 'en tittel og her',
-          description:
-            'en liten beskrivelse her daasd asd asd asdassad af aert yfd zf hsrthxgh s cgh srs ghsrt xghsth altså',
-        },
-      ],
+      articles,
     };
   },
 };
