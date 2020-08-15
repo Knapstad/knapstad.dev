6adfa4d389f6707c0dba92c44bf8785a8619a024
Bendik <knaben@OBOS.NO>
2020-08-15 11:56:52 +0200

    added date and link to article

--- src/components/ArticlePreview.vue
+++ src/components/ArticlePreview.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="cardbg">
-    <h2>{{ article.title | capitalize }}</h2>
-    <p>{{ article.description | capitalize }}</p>
+    <a :href="article.path" class="cardlink">
+      <h2>{{ article.title | capitalize }}</h2>
+      <p class="date">Created on: {{ article.date }}</p>
+      <p>{{ article.description | capitalize }}</p>
+      <a class="readmore" v-bind:href="article.path">
+        <div v-bind:href="article.path" class="readmore">Read more</div></a
+      >
+    </a>
   </div>
 </template>
 
@@ -15,12 +21,26 @@ export default {
 h2 {
   text-align: left;
   margin: 1px;
-  padding: 2px;
+  padding-top: 8px;
+  padding-left: 5px;
+  padding-bottom: 0;
+  font-size: 18 px;
 }
 p {
   text-align: left;
   margin-left: 2px;
+  padding: 5px;
   padding-bottom: 5px;
+}
+p.date {
+  margin-top: 1px;
+  text-align: left;
+  padding-top: 0;
+  padding-left: 5px;
+  padding-bottom: 3px;
+  font-size: 10px;
+  font-family: consolas;
+  color: gray;
 }
 .cardbg {
   background: whitesmoke;
@@ -30,5 +50,35 @@ p {
   width: 100%;
   -webkit-margin-after: 1rem;
   margin-block-end: 1rem;
+}
+.cardlink {
+  color: initial;
+  text-decoration: none;
+}
+a.readmore {
+  display: flex;
+  width: 100%;
+  border: 0;
+  height: 40px;
+  padding: 0;
+  margin: 0;
+  font-size: 20px;
+  background: gray;
+  text-decoration: none;
+  color: initial;
+  appearance: button;
+  text-align: center;
+}
+div.readmore {
+  margin: auto;
+}
+.readmore:hover {
+  background: lightgray;
+}
+.readmore:focus {
+  border: 2px solid;
+  font-size: 23px;
+  width: 99.9%;
+  overflow: hidden;
 }
 </style>
