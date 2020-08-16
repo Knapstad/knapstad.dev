10f7ee5353b216007f71556fd9b7017d056362a5
Bendik <knaben@OBOS.NO>
2020-08-16 14:08:14 +0200

    added blogg wrapper view

---
+++ src/views/Blogg.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <component :is="componentFile" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  computed: {
+    componentFile() {
+      return () => import(`@/assets/Atricles/${this.data.path}.md`);
+    },
+  },
+};
+// console.log(this.data.path);
+console.log('What');
+</script>
+
+<style></style>
