9665fe1416227a2b25bcdfba9131214b841a6a4b
Bendik <knaben@OBOS.NO>
2020-08-16 14:17:39 +0200

    remove vue headfull

--- src/views/Gtmversionchecker copy.vue
+++ src/views/Gtmversionchecker copy.vue
@@ -1,9 +1,5 @@
 <template>
   <main>
-    <vue-headful
-      title="Google tag manager Version checker"
-      description="Integrate Google Tag Manager changes with Microsoft Teams and Slack "
-    />
     <div>hei hei</div>
     <div>
       <h2>hei</h2>
