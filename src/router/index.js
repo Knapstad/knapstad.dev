0d42d972248be5ce6fd00173596a20919733aa9a
Bendik <knaben@OBOS.NO>
2020-08-16 14:10:00 +0200

    added vue meta

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueMeta from 'vue-meta';
 import routes from '@/router/routes';
 
 Vue.use(VueRouter);
+Vue.use(VueMeta);
 
 const router = new VueRouter({
   mode: 'history',
