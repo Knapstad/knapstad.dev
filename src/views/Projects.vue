0de180c7adb2b09975123c7c6e477d39602c5da4
Bendik Knapstad <knaben@OBOS.NO>
2020-11-06 10:14:10 +0100

    added projects.vue view,

---
+++ src/views/Projects.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-for="project in projects" :key="project.id">
+      <ArticlePreview v-bind:article="project" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticlePreview from '@/components/ArticlePreview.vue';
+import projects from '@/assets/projects/projects.json';
+
+export default {
+  name: 'Projects',
+  components: { ArticlePreview },
+  metaInfo() {
+    return {
+      title: 'Knapstad.dev - ',
+      titleTemplate: `%s | Projects`,
+      meta: [
+        {
+          name: 'description',
+          content: `A list of my projects`,
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      projects,
+    };
+  },
+};
+</script>
+<style scoped></style>
