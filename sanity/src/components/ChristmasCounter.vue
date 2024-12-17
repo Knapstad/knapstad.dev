a131f4a7e7ca0936b0d6693419ae6821adda56d5
Bendik <bendik.knapstad@obos.no>
2024-12-17 20:58:46 +0100

    add more emojis

--- sanity/src/components/ChristmasCounter.vue
+++ sanity/src/components/ChristmasCounter.vue
@@ -1,20 +1,20 @@
 <template>
   <div v-if="date === 'day'" class="counter">
     <div v-if="today == '12/23/' + thisYear">
-      <p>Nå er det lille julaften! Husk å se Gevinnen og hovmesteren</p>
+      <p>Nå er det lille julaften! Husk å se Gevinnen og hovmesteren klokka 21</p>
     </div>
     <div v-else-if="Date(today) >= new Date('12/24/' + thisYear)">
-      <p>Nå er det jul!</p>
+      <p class="antall">Nå er det jul!</p>
     </div>
     <div v-else-if="days < 50">
       <p>Oj oj oj nå er det ikke lenge igjen til jul!</p>
       <p>
-        Det er bare <strong>{{ days }}</strong> dager igjen!
+        Det er bare <strong class="antall">{{ days }}</strong> dager igjen!
       </p>
     </div>
     <div v-else onload="$emit('dateObject',day)">
       <p>
-        Det er <strong>{{ days }}</strong> dager igjen til jul.
+        Det er <strong class="antall">{{ days }}</strong> dager igjen til jul.
       </p>
     </div>
   </div>
@@ -31,12 +31,12 @@
     <div v-else-if="days < 50">
       <p>Oj oj oj nå er det ikke lenge igjen til jul!</p>
       <p>
-        Det er bare <strong>{{ weeks }}</strong> uker igjen!
+        Det er bare <strong class="antall">{{ weeks }}</strong> uker igjen!
       </p>
     </div>
     <div v-else>
       <p>
-        Det er <strong>{{ weeks }}</strong> uker igjen til jul.
+        Det er <strong class="antall">{{ weeks }}</strong> uker igjen til jul.
       </p>
     </div>
   </div>
@@ -45,29 +45,27 @@
 
 <script>
 export default {
-   /* eslint-disable */
   data() {
-    let thisYear = new Date().getFullYear();
+    const thisYear = new Date().getFullYear();
     return {
       thisYear: thisYear,
       today: new Date().toLocaleDateString(),
-      days: (this.returnDateJson(`12/24/` + thisYear).days / 1) >> 0,
-      weeks: (this.returnDateJson('12/24/' + thisYear).weeks / 1) >> 0,
+      days: (this.returnDateJson(`12/24/${thisYear}`).days / 1) >> 0,
+      weeks: (this.returnDateJson(`12/24/${thisYear}`).weeks / 1) >> 0,
     };
   },
   mounted: function() {
-    // console.log('joda');
     this.$nextTick(function() {
       this.$emit('dateObject', { days: this.days, weeks: this.weeks });
     });
   },
   methods: {
-    returnDateJson: function(date) {
+    returnDateJson: (date) => {
       console.log(date);
-      let today = new Date();
-      let calcDate = new Date(date);
-      let difference = calcDate.getTime() - today.getTime();
-      let dateObject = {
+      const today = new Date();
+      const calcDate = new Date(date);
+      const difference = calcDate.getTime() - today.getTime();
+      const dateObject = {
         days: difference / (1000 * 60 * 60 * 24),
         weeks: difference / (1000 * 60 * 60 * 24) / 7,
       };
@@ -85,7 +83,11 @@ export default {
   border-radius: 1px;
   box-shadow: 2px 2px 5px #000;
   width: 100%;
-  /* -webkit-margin-after: 1rem; */
   padding: 0.5rem;
+}
+.antall {
+  position: relative;
+  z-index: 500;
+  -webkit-text-stroke: .2px white;
 }
 </style>
